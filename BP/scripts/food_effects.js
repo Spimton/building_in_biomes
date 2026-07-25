8572e2b8fba81199faa6deb1d016bbef95abe3c0
Spimton <spimton.fictional@gmail.com>
2026-07-25 18:08:09 +0200

    Building in Biomes 1.1.25.07

--- BP/scripts/food_effects.js
+++ BP/scripts/food_effects.js
@@ -655,7 +655,7 @@ world.afterEvents.itemCompleteUse.subscribe(event => {
     const { source: entity, itemStack } = event
     if (entity.typeId !== "minecraft:player" || !itemStack || !bottledtime.includes(itemStack.typeId)) return;
     entity.runCommand("scoreboard objectives add pharaohcursespimton dummy")
-    entity.runCommand("execute if @s[scores={spimton:sandals=1}] runscoreboard players add @s pharaohcursespimton 400")
+    entity.runCommand("execute if entity @s[scores={spimton:sandals=1}] run scoreboard players add @s pharaohcursespimton 400")
 })
 
 world.afterEvents.itemCompleteUse.subscribe(event => {
